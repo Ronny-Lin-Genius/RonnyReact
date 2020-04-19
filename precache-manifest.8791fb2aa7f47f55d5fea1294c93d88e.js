d87e194b9af8827fcce07c00c128a8df03f07caf
Ronny_Lin <jdwx@JDWXdeMacBook-Air.local>
2020-04-19 22:14:39 +1000

    Updates

---
+++ precache-manifest.8791fb2aa7f47f55d5fea1294c93d88e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14df0126d267ace56588c43e7c0300d",
+    "url": "/RonnyReact/index.html"
+  },
+  {
+    "revision": "60ede984b334cf53abf7",
+    "url": "/RonnyReact/static/css/main.c949d7fb.chunk.css"
+  },
+  {
+    "revision": "92884018adebf540df07",
+    "url": "/RonnyReact/static/js/2.639bbab3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/RonnyReact/static/js/2.639bbab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ede984b334cf53abf7",
+    "url": "/RonnyReact/static/js/main.cbbc9fb9.chunk.js"
+  },
+  {
+    "revision": "2102bbd86a09c026b153",
+    "url": "/RonnyReact/static/js/runtime-main.8f164d32.js"
+  },
+  {
+    "revision": "fa962a88076d633b5c32f18c192505ab",
+    "url": "/RonnyReact/static/media/coronavirus.fa962a88.jpg"
+  }
+]);
